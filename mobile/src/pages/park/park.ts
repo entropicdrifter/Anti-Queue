8fdf644c848f280081f4ef02bdbfa2ccd72a6b56
Joe <mrjoeroddy@gmail.com>
2017-08-19 11:02:13 -0400

    render ride list w/times & colored icons

--- mobile/src/pages/park/park.ts
+++ mobile/src/pages/park/park.ts
@@ -9,7 +9,7 @@ import { Rides } from '../../service/Rides';
 export class Park {
     park: string;
     icons: string[];
-    items: Array<{ title: string, note: string, icon: string }>;
+    items: Array<{ title: string, note: string, icon: string, color: string }>;
 
     constructor(public navCtrl: NavController, public navParams: NavParams) {
         // Let's populate this page with some filler content for funzies
@@ -17,21 +17,51 @@ export class Park {
             'american-football', 'boat', 'bluetooth', 'build'];
         // If we navigated to this page, we will have an item available as a nav param
         this.park = navParams.get('parkId');
-        this.park = "KingsIsland";
+        // this.park = "KingsIsland";
         let ride = new Rides();
-        ride.getRides("KingsIsland", res => {
+        ride.getRides(this.park, res => {
             console.log("Rides:", res);
             this.items = [];
-            debugger
-            let rides = res.slice(0, 10);
-            rides.forEach((ride)=>{
+            res && res.forEach((ride) => {
+                let iconInfo = this.getIcon(ride.status);
                 this.items.push({
                     title: ride.name,
-                    note: ride.status,
-                    icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+                    note: ride.status === "Operating" ?
+                        ride.waitTime + " minute wait" : ride.status,
+                    icon: iconInfo.icon,
+                    color: iconInfo.color
                 });
             })
         })
+    }
+
+    getWaitTimeString(waitTime) {
+        return
+    }
+
+    getIcon(status) {
+        switch (status) {
+            case "Refurbishment":
+                return {
+                    icon: "ios-construct",
+                    color: "red"
+                };
+            case "Closed":
+                return {
+                    icon: "ios-close-circle-outline",
+                    color: "red"
+                };
+            case "Down":
+                 return {
+                    icon: "ios-build",
+                    color: "red"
+                };
+            default:
+                return {
+                    icon: "ios-checkmark-circle-outline",
+                    color: "green"
+                };
+        }
     }
 
     itemTapped(event, item) {
