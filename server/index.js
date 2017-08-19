03b0fa4e90c2070df37b0a4abe94cf8837ade774
Joe <mrjoeroddy@gmail.com>
2017-08-19 08:34:02 -0400

    basic server for getting parks, ride times, and operation schedules

---
+++ server/index.js
@@ -0,0 +1,35 @@
+var Themeparks = require("themeparks");
+const express = require("express");
+const app = express();
+
+var allParks = {};
+for (var park in Themeparks.Parks) {
+    var p = {};
+    var parkObj = new Themeparks.Parks[park]();
+    p.name = parkObj.Name;
+    p.timezone = parkObj.Timezone;
+    p.location = parkObj.Location.toString();
+    allParks[park] = p; 
+}
+
+app.get('/getParks', function (req, res) {
+    res.send(allParks);
+})
+
+app.get('/getParkOpeningTimes/:parkId', function (req, res) {
+    var park = new Themeparks.Parks[req.params.parkId]();
+    park.GetOpeningTimes().then(function (times) {
+        res.send(times);
+    }, console.error);
+})
+
+app.get('/getRides/:parkId', function (req, res) {
+    var park = new Themeparks.Parks[req.params.parkId]();
+    park.GetWaitTimes().then(function (rides) {
+        res.send(rides);
+    }, console.error);
+})
+
+app.listen(3000, function () {
+  console.log('app listening on port 3000!')
+})
